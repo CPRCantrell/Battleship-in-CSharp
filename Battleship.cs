d9b371016ff8db9dfb30ce00617144e99ec80253
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-06-06 15:18:05 -0400

    📦 NEW: allow to play again and validates entry

--- Battleship.cs
+++ Battleship.cs
@@ -12,7 +12,39 @@ class Battleship
 
     private void Welcome()
     {
+        string welcomeTitle = "- Welcome to BattleShip -";
+        Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (welcomeTitle.Length / 2)) + "}", welcomeTitle));
+        Console.WriteLine("\nBattleship is a classic game of setting up your board with your fleet and go to war against an opponent. Each player will take turns naming corrdinates in hopes of striking your Oppnents ship,and once struck you will get another turn until you miss. Plan carefully and Have fun!");
+        Console.Write("\n\nPress ENTER to start...");
+        while(Console.ReadKey(true).Key != ConsoleKey.Enter)
+            continue;
+    }
 
+    private bool PlayAgain()
+    {
+        string[] acceptableYes = {"again", "yes", "yea", "y"};
+        string[] acceptableNo = {"done", "no", "naw", "n"};
+
+        while(true)
+        {
+            Console.Write("Would you like to play again: ");
+            try
+            {
+                string response = Console.ReadLine().Split(" ").Where(character => character != "").ToArray()[0].ToLower();
+                Console.WriteLine(response);
+
+                foreach(string answer in acceptableYes)
+                    if(answer == response)
+                        return true;
+
+                foreach(string answer in acceptableNo)
+                    if(answer == response)
+                        return false;
+            }
+            catch(IndexOutOfRangeException){}
+
+            Console.WriteLine($"Invalid Entry! Acceptable Answers are [{string.Join(", ", acceptableYes)}] or [{string.Join(", ", acceptableNo)}]");
+        }
     }
 
     private void GameLoop()
@@ -20,8 +52,4 @@ class Battleship
 
     }
 
-    private bool PlayAgain()
-    {
-        return false;
-    }
 }
