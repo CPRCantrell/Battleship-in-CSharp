893a5a76ba02c2e3e5ff52ab5ce1230b81ccc11c
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-06-06 11:17:30 -0400

    📦 NEW: Able to fillter menu options from future inputs

--- OptionMenu.cs
+++ OptionMenu.cs
@@ -1,11 +1,38 @@
 class OptionMenu
 {
-    private string[] MenuOptions = {"menu", "options", "option", "m", "o"};
+    private string[] MenuOptions = {"menu", "options", "option", "help", "m", "o"};
     private string[] QuitOptions = {"quit", "leave", "q"};
     private string[] RestartOptions = {"restart", "r"};
 
-    public string filterInput()
+    public string filterInput(string input)
     {
+        foreach(string option in MenuOptions)
+            if(input.ToLower() == option)
+                this.Menu();
+
+        foreach(string option in QuitOptions)
+            if(input.ToLower() == option)
+                this.Quit();
+
+        foreach(string option in RestartOptions)
+            if(input.ToLower() == option)
+                this.Restart();
+
         return "string";
+    }
+
+    private void Menu()
+    {
+        Console.WriteLine("Menu");
+    }
+
+    private void Quit()
+    {
+        Console.WriteLine("Quit");
+    }
+
+    private void Restart()
+    {
+        Console.WriteLine("Restart");
     }
 }
